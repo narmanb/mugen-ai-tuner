5081aa82797ac133f2f2008887589de6cfa44642
narmanb <43427228+narmanb@users.noreply.github.com>
2026-08-31 03:33:29 -0500

    Test AILevel on-off versus numeric scaling

---
+++ core/src/test/kotlin/com/narmanb/mugenaituner/core/AiLevelDifficultyScalingTest.kt
@@ -0,0 +1,32 @@
+package com.narmanb.mugenaituner.core
+
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class AiLevelDifficultyScalingTest {
+    @Test
+    fun pureAiOnOffChecksAreNotNumericScaling() {
+        assertFalse(AiLevelDifficultyScaling.hasNumericScaling("trigger1 = AILevel > 0"))
+        assertFalse(AiLevelDifficultyScaling.hasNumericScaling("trigger1 = AILevel >= 1"))
+        assertFalse(AiLevelDifficultyScaling.hasNumericScaling("trigger1 = AILevel != 0"))
+        assertFalse(AiLevelDifficultyScaling.hasNumericScaling("trigger1 = AILevel"))
+        assertFalse(AiLevelDifficultyScaling.hasNumericScaling("trigger1 = AILevel = [1,8]"))
+    }
+
+    @Test
+    fun levelDiscriminatingComparisonsAreNumericScaling() {
+        assertTrue(AiLevelDifficultyScaling.hasNumericScaling("trigger1 = AILevel = 1"))
+        assertTrue(AiLevelDifficultyScaling.hasNumericScaling("trigger1 = AILevel >= 4"))
+        assertTrue(AiLevelDifficultyScaling.hasNumericScaling("trigger1 = AILevel < 5"))
+        assertTrue(AiLevelDifficultyScaling.hasNumericScaling("trigger1 = AILevel = [4,8]"))
+        assertTrue(AiLevelDifficultyScaling.hasNumericScaling("trigger1 = 3 < AILevel"))
+    }
+
+    @Test
+    fun arithmeticUseIsNumericScaling() {
+        assertTrue(AiLevelDifficultyScaling.hasNumericScaling("trigger1 = Random < AILevel * 100"))
+        assertTrue(AiLevelDifficultyScaling.hasNumericScaling("trigger1 = Random < 100 * AILevel"))
+        assertTrue(AiLevelDifficultyScaling.hasNumericScaling("value = AILevel / 8.0"))
+    }
+}
